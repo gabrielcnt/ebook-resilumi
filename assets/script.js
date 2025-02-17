7763ac20903acf6651cc2fa33ba644fe70a58a4b
gabriel vieira <gabrielcnt96@gmail.com>
2025-02-17 15:58:58 -0300

    media quere finalizado

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+function animar(){
+    const btn = document.getElementById('btn-menu')
+    const menu = document.getElementById('menubt')
+    
+    btn.classList.toggle('ativar')
+    menubt.classList.toggle('ativar')
+}
